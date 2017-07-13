70ec0911cf5f03939e23aad5b87786f9ff27a09b
Laura Senko <laurasenko@gmail.com>
2017-07-13 15:25:44 -0400

    first commit Lab 5 Senko

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log ("working");
+
+var tempFar = 45
+var tempCel = 8
+
+tempCel = (tempFar - 32) * (5/9);
+
+console.log (tempFar + " degrees Fahrenheit is "  + tempCel  +  "degrees Celsius.");
+
+tempFar = (tempCel * 9/5) + 32;
+
+console.log (tempCel + "degrees Celsius is " + tempFar + "degrees Fahrenheit.")
